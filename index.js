165ccbe5a938450345578279a0cb10a74fc870d5
Bijaylaxmi Behera <72284560+bijaylaxmibehera@users.noreply.github.com>
2022-11-07 20:42:48 +0530

    feat: js added

---
+++ index.js
@@ -0,0 +1,16 @@
+const ratingContainer = document.querySelector(".rating-card");
+const thankContainer = document.querySelector(".thank-you-card");
+const submitBtn = document.querySelector("#submit-rating");
+const ratings = document.querySelectorAll(".btn");
+const actualRating = document.querySelector("#rating");
+
+ratings.forEach((rating) => {
+    rating.addEventListener("click", () => {
+        actualRating.innerHTML = rating.innerHTML
+    })
+})
+
+submitBtn.addEventListener("click", () => {
+    ratingContainer.style.display = "none";
+    thankContainer.classList.remove("hidden")
+})
